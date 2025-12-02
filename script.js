2d09d6fd9ef93b50f898146aa0168a7e559240e7
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-02 12:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const checkboxes = document.querySelectorAll(".checkbox");
+let checkedCount = 0;
+let checkedArray = [];
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      checkedCount++;
+      checkedArray.push(checkbox);
+      if (checkedCount > 2) {
+        const firstChecked = checkedArray.shift();
+        firstChecked.checked = false;
+        checkedCount--;
+      }
+    } else {
+      checkedCount--;
+      checkedArray = checkedArray.filter((cb) => cb !== checkbox);
+    }
+  });
+});
